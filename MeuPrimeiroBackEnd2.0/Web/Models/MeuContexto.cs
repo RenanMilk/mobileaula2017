f6f6805e29f643c2b14132aff90bc1a3be8a6869
1613719 <renanvieiraleite@gmail.com>
2017-10-23 15:34:37 -0200

    Criando Contexto

---
+++ MeuPrimeiroBackEnd2.0/Web/Models/MeuContexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Web.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+    }
+}
