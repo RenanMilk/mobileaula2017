3d86041c6020d9694c37c98ace83b29688a03c4e
1613719 <renanvieiraleite@gmail.com>
2017-10-23 15:44:01 -0200

    Criando as modelos (classes das tabelas)

---
+++ MeuPrimeiroBackEnd2.0/Web/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using Web.Models;
+namespace Web.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Raridade { get; set; }
+        public int DanoMaximo { get; set; }
+        //Relacionamento item -- TipoItem
+        public int TipoItemID { get; set; }
+        public virtual TipoItem _TipoItem { get; set; }//evita o NullReference.
+        //para fazer o lazy loading.
+    }
+}
---
+++ MeuPrimeiroBackEnd2.0/Web/Models/TipoItem.cs
@@ -0,0 +1,10 @@
+﻿using Web.Models;
+
+namespace Web.Models
+{
+    public class TipoItem
+    {
+        public int TipoItemID { get; set; }
+        public int Nome { get; set; }
+    }
+}
